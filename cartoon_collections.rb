f29e7d00ba264d43c27f4949a92b51cee6d52ced
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-23 06:10:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,10 @@ def summon_captain_planet(calls)
   calls.map {|planeteer| planeteer.capitalize.insert(-1, "!") }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each {|planeteer| if planeteer.length > 4 true }
+  else
+    false
 end
 
 def find_the_cheese# code an argument here
