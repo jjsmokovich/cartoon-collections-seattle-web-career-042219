3a5ce158e04c7f0a2fc9da54d699b367520d83ef
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-23 07:30:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,8 @@ def long_planeteer_calls(calls)
   calls.any? {|planeteer| planeteer.length > 4 }
 end
 
-def find_the_cheese
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  snacks.any? {|food| food.include?(cheese_types)}
 end
